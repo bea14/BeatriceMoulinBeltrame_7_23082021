952d47ba04bf03cdae8b0332d27b8e98c634dc79
bea14 <58847427+bea14@users.noreply.github.com>
2021-10-01 19:13:10 +0200

    Suppression du composant Header

--- groupomania/frontend/src/App.vue
+++ groupomania/frontend/src/App.vue
@@ -1,34 +1,15 @@
 <script>
-import Header from "./components/layout/LayoutHeader.vue";
 import Footer from "./components/layout/LayoutFooter";
 export default {
   name: "App",
-  data(){
-    return {
-      headerKey: 0,
-    }
-  },
   components: {
-    Header,
     Footer,
   },
-  methods: {
-      showPage() {
-      console.log(window.location.pathname.split('/'));
-      let page = true;
-      let path = window.location.pathname;
-      if (path == "/") {
-        page = false;
-      }
-      return page;
-    },
-  }
 };
 </script>
 
 <template>
-  <div id="app">    
-    <Header @gotohome="path = '/Posts'" v-if="showPage()" :key="headerKey"/>
+  <div id="app"> 
     <main>
       <router-view />
     </main>
@@ -37,14 +18,9 @@ export default {
 </template>
 
 <style lang="scss">
-@import "./assets/styles/utils/_variables.scss";
-#app {
-  font-family: Verdana, Arial, Helvetica, sans-serif;
-}
 main {
-  background-color: lighten($tertiary-color, 50%);
-  margin-top: 8rem;
-  @media screen and (min-width: 770px) {
+  margin-top: 20vh;
+  @media screen and (min-width: 668px) {
     margin-top: 4rem;
   }
 }
